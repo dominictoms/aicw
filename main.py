57606233b00231e14b4ffb3a13eac6472af4ad26
Dominic Schmidt-Toms <dom@macbook.local>
2024-05-11 15:33:24 +0100

    massive cleanup

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import math
 import sys
 import json
 import re
-
 from datetime import time, datetime, timedelta
 from dateutil.parser import parse
 from threading import Thread
@@ -34,7 +33,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 # hacky fix to remove flask opening message
-cli = sys.modules['flask.cli']
+cli = sys.modules["flask.cli"]
 cli.show_server_banner = lambda *x: None
 
 # make flask and socketio
@@ -43,50 +42,51 @@ app.logger.disabled = True
 socketio = SocketIO(app, logger=False)
 
 # don't spam console with this stuff
-logging.getLogger('socketio').setLevel(logging.ERROR)
-logging.getLogger('engineio').setLevel(logging.ERROR)
-logging.getLogger('werkzeug').setLevel(logging.ERROR)
+logging.getLogger("socketio").setLevel(logging.ERROR)
+logging.getLogger("engineio").setLevel(logging.ERROR)
+logging.getLogger("werkzeug").setLevel(logging.ERROR)
 
 # global list of conversation history, for web server
 history = []
 
+# TODO feels wrong to have this global
+trainPredict = None
+
 # route the root of the web server to index.html
-@app.route('/')
+@app.route("/")
 def index():
-	return render_template('index.html')
+	return render_template("index.html")
 
 # populate ui with chat history on connection
-@socketio.on('connect')
-def handle_connection():
+@socketio.on("connect")
+def handleConnection():
 	for message in history:
-		socketio.emit('message', message)
+		socketio.emit("message", message)
 
-# when the user 
-@socketio.on('message')
-def handle_message(message):
+# when the user sends a message from the gui 
+@socketio.on("message")
+def handleMessage(message):
 	trainPredict.webInput(message)
 	trainPredict.newQuery(message)
 
 def main():
-
 	# start a new query
-	trainPredict.new_query()
-	
+	trainPredict.newQuery()
 
 @Language.component("info_semantic")
-def info_semantic(doc):
+def infoSemantic(doc):
 
 	# TODO pike comment this i have no idea what this does
-	train_predict = TrainPredict.get_instance()
+	trainPredictObject = TrainPredict.getInstance()
 	for token in doc:
-		if token.text.lower() in train_predict.info_words:
+		if token.text.lower() in trainPredictObject.infoWords:
 			token._.identical = True
-			token._.info = train_predict.info_words[token.text.lower()]
+			token._.info = trainPredictObject.infoWords[token.text.lower()]
 		else:
-			for word, info_token in train_predict.info_tokens.items():
-				if token.has_vector and token.similarity(info_token) > train_predict.sim_thresh:
+			for word, infoToken in trainPredictObject.infoTokens.items():
+				if token.has_vector and token.similarity(infoToken) > trainPredictObject.simThresh:
 					token._.identical = False
-					token._.info = train_predict.info_words[word]
+					token._.info = trainPredictObject.infoWords[word]
 					break
 	return doc
 
@@ -95,9 +95,9 @@ class TrainPredict:
 	# trainpredict is a singleton i guess
 	_instance = None
 
-	# static method to get the current instance of trainpredict
+	# this does some singleton related stuff idk
 	@staticmethod
-	def get_instance():
+	def getInstance():
 		if TrainPredict._instance is None:
 			TrainPredict()
 		return TrainPredict._instance
@@ -109,23 +109,26 @@ class TrainPredict:
 			raise Exception("trainpredict is a singleton!")
 		else:
 			TrainPredict._instance = self
+
+		# let her send the opening line ;)
+		self.chatbotSpeech("Welcome to Trainrunner! How may I help xx")
 		
 		# set up spacy
 		self.nlp = spacy.load("en_core_web_md")
 		
 		# custom tokens for info items and identical items
-		Token.set_extension('info', default=None, force=True)
-		Token.set_extension('identical', default=None, force=True)
+		Token.set_extension("info", default=None, force=True)
+		Token.set_extension("identical", default=None, force=True)
    
 		# add the station ruler to spacy before named entity recognition 
-		self.stationRuler = self.nlp.add_pipe("entity_ruler", before='ner')
-		self.load_stations()
+		self.stationRuler = self.nlp.add_pipe("entity_ruler", before="ner")
+		self.loadStations()
 		
 		# add custom component for semantic analysis by registered name
 		self.nlp.add_pipe("info_semantic", after="entity_ruler")
 		
 		# key semantic words and their roles
-		self.info_words = {
+		self.infoWords = {
 
 			# the very simple words, like to and from
 			"to": [["to", 1, "STATION"]],
@@ -189,23 +192,23 @@ class TrainPredict:
 		}
 		
 		# preparing a matcher for semantic similarity
-		self.prepare_info_tokens()
+		self.prepareInfoTokens()
 
 		# similarity threshold
-		self.sim_thresh = 0.6
+		self.simThresh = 0.6
 
 	# function for setting info_tokens variable
-	def prepare_info_tokens(self):
-		self.info_tokens = {
-			word: self.nlp(word)[0] for word in self.info_words if self.nlp(word)[0].has_vector
+	def prepareInfoTokens(self):
+		self.infoTokens = {
+			word: self.nlp(word)[0] for word in self.infoWords if self.nlp(word)[0].has_vector
 		}
 
 	# load all the stations from the csv file
-	def load_stations(self):
+	def loadStations(self):
 		
 		# read the csv file
-		script_dir = os.path.dirname(os.path.abspath(__file__))
-		with open(os.path.join(script_dir, "stations.csv"), 'r') as file:
+		scriptDir = os.path.dirname(os.path.abspath(__file__))
+		with open(os.path.join(scriptDir, "stations.csv"), "r") as file:
 
 			# put all the lines in a list
 			lines = file.readlines()
@@ -217,14 +220,14 @@ class TrainPredict:
 			for line in lines:
 
 				# clean the line up
-				line = line.strip().replace('"', '')
+				line = line.strip().replace("\"", "")
 
 				# if the line is empty, give it a miss
 				if not line:
 					continue
 
 				# extract all the info from the csv
-				parts = line.split(',')
+				parts = line.split(",")
 				name, longname, tiploc = parts[0], parts[1], parts[3]
 
 				# add the station longname and name to the station ruler, tiploc as id
@@ -245,44 +248,64 @@ class TrainPredict:
 					}
 				])
 
-	# user messages chatbox from cli
+	# user messages chatbot from cli
 	def cliInput(self):
+
+		# get message from cli and add users name
 		message = input()
-		socketio.emit('message', f"User: {message}")
+		message = f"User: {message}"
+
+		# output to gui (will stay in cli from input)
+		socketio.emit("message", f"User: {message}")
+
+		# add to history
 		history.append("User: " + message)
+
+		# send it back
 		return message
 
-	# user messages chatbox from web
+	# user messages chatbot from web
 	def webInput(self, message):
-		print(f"User: {message} ")
-		socketio.emit('message', f"User: {message}")
-		history.append("User: " + message)
 
-	# chatbox messages user
+		# add the users name to the message
+		message = f"User: {message}"
+
+		# output to cli and gui
+		print(message)
+		socketio.emit("message", message)
+
+		# add to history
+		history.append(message)
+
+	# chatbot messages user
 	def chatbotSpeech(self, message):
-		print(f"TrainRunner: {message} ")
-		socketio.emit('message', f"TrainRunner: {message}")
-		history.append("TrainRunner: " + message)
+
+		# add the chatbots name to the message
+		message = f"TrainRunner: {message} "
+
+		# output to cli and gui
+		print(f"{message} ")
+		socketio.emit("message", message)
+
+		# add to history
+		history.append(message)
 	
 	# function to start a new query
-	def new_query(self, query=None):
+	def newQuery(self, query = None):
 
 		# boolean to check if message was sent via cli or server
 		webRequest = query != None
 		
-		# chatbot says hi!
-		self.chatbotSpeech("Hello! How can I help?")
-
 		# get input from cli if no query is provided
 		if not webRequest:
 			query = self.cliInput()
 
-		#TODO call the output function when ui is more stable
-		#complete = self.output(query)
+		# call the output function!
+		self.output(query)
 
 		# infinite loop the query prompt for the cli
 		if not webRequest:
-			self.new_query()
+			self.newQuery()
 
 	# actually process the query
 	def processQuery(self, query):
@@ -292,7 +315,7 @@ class TrainPredict:
 
 		# list of which variables we're certain on
 		certain = [None,None,None,None,None,None,None]
-		to_station, from_station, journey_date, journey_time, adultTickets, childTickets = None, None, None, None, None, None
+		toStation, fromStation, journeyDate, journeyTime, adultTickets, childTickets = None, None, None, None, None, None
 
 		# this works wonders but i cannot comprehend what's happening here
 		skip = False
@@ -301,28 +324,26 @@ class TrainPredict:
 				if token._.info:
 					for meaning in token._.info:
 						if meaning[1] >= 1:
-							data_pos = token.i + 1
+							dataPos = token.i + 1
 						elif meaning[1] == -1:
-							data_pos = token.i - 1
-						if meaning[1] == 2 and data_pos < len(doc):
+							dataPos = token.i - 1
+						if meaning[1] == 2 and dataPos < len(doc):
 								meaning[1] -= 1
 								if isinstance(meaning[0], list):
-									if doc[data_pos].text == meaning[0][0]:
+									if doc[dataPos].text == meaning[0][0]:
 										meaning[0] = meaning[0][1]
-										data_pos += 1
+										dataPos += 1
 										skip = True
 									else:
 										continue
 								else:
-									if doc[data_pos].text == meaning[0]:
-										data_pos += 1
+									if doc[dataPos].text == meaning[0]:
+										dataPos += 1
 										skip = True
 									else:
 										continue
-						isNum = "NUM" if doc[data_pos].like_num else "NOTNUM"
-						if data_pos < len(doc) and (doc[data_pos].ent_type_ == meaning[2] or isNum == meaning[2] or 0== meaning[1]): 
-							print(meaning)
-							print(f"len{len(meaning)}")
+						isNum = "NUM" if doc[dataPos].like_num else "NOTNUM"
+						if dataPos < len(doc) and (doc[dataPos].ent_type_ == meaning[2] or isNum == meaning[2] or 0== meaning[1]): 
 							if len(meaning) == 4:
 								certain[6] = True
 							if meaning[0] == "from":
@@ -330,28 +351,28 @@ class TrainPredict:
 									certain[0] = True
 								else:
 									certain[0] = False
-								from_station = doc[data_pos].ent_id_ 
+								fromStation = doc[dataPos].ent_id_ 
 								break
 							elif meaning[0] == "to":
 								if token._.identical:
 									certain[1] = True
 								else:
 									certain[1] = False
-								to_station = doc[data_pos].ent_id_
+								toStation = doc[dataPos].ent_id_
 								break
 							elif meaning[0] == "on":
 								if token._.identical:
 									certain[2] = True
 								else:
 									certain[2] = False
-								journey_date = parse(doc[data_pos].text).date()
+								journeyDate = parse(doc[dataPos].text).date()
 								break
 							elif meaning[0] == "by":
 								if token._.identical:
 									certain[3] = True
 								else:
 									certain[3] = False
-								journey_time = self.to_time(doc[data_pos].text)
+								journeyTime = self.toTime(doc[dataPos].text)
 								break
 							
 							elif meaning[0] == "adult":
@@ -359,100 +380,136 @@ class TrainPredict:
 									certain[4] = True
 								else:
 									certain[4] = False
-								adultTickets = w2n.word_to_num(doc[data_pos].text)
+								adultTickets = w2n.word_to_num(doc[dataPos].text)
 								break
 							elif meaning[0] == "child":
 								if	token._.identical:
 									certain[5] = True
 								else:
 									certain[5] = False
-								childTickets = w2n.word_to_num(doc[data_pos].text)
-								print(certain[5])
+								childTickets = w2n.word_to_num(doc[dataPos].text)
 								break
-							
 
 			else:
 				skip = False
 
 		# we're currently assuming if no date is provided, do it right now
-		current_datetime = datetime.now()
-		if journey_date == None:
-			journey_time = current_datetime.time()
+		currentDatetime = datetime.now()
+		if journeyDate == None:
+			journeyTime = currentDatetime.time()
 			certain[3] == False
 
-		if journey_date == None:
-			journey_date = current_datetime.date() 
+		if journeyDate == None:
+			journeyDate = currentDatetime.date() 
 			certain[3] == False
 
 		# send it all back
-		return(certain, to_station, from_station, journey_date, journey_time, adultTickets, childTickets)
+		return certain, toStation, fromStation, journeyDate, journeyTime, adultTickets, childTickets
 
 	# generate the response to the users query	
 	def output(self, query):
 		
-		# go on forever and forever
-		while True:
-			certain, to_station, from_station, journey_date, journey_time, adultTickets,childTickets = self.processQuery(query)
-			uncertain = ""
-			unknown = ""
-			output = ""
-			for i, cat in enumerate(certain):
-				#print(cat)
-				if cat == False:
-					if i == 0:
-						uncertain += f"from {from_station} "
-					elif i == 1:
-						uncertain += f"to {to_station} "
-					elif i == 2:
-						uncertain += f"on {journey_date} "
-					elif i == 3:
-						uncertain += f"by {journey_time} "
-					elif i == 4:
-						uncertain += f"for {adultTickets} adult(s) "
-					elif i == 5:
-						uncertain += f"for {childTickets} child(ren) "
-					elif i == 6:
-						uncertain += f"and {'youd like to return 'if certain[6] else 'you would not like to return'}"
-				elif cat == None:
-					if i == 0:
-						unknown += "from which station \n"
-					elif i == 1:
-						unknown += "to which station  \n"
-					elif i == 2:
-						unknown += "what date \n"
-					elif i == 3:
-						unknown += "by what time \n "
-					elif i == 4:
-						unknown += "how many adult(s) \n"
-					elif i == 5:
-						unknown += "how many child(ren) \n"
-					elif i == 6:
-						unknown += "would you like to return \n"
-			print(certain)
-			if uncertain != "":
-				output += ("just to check you want to go " + uncertain)
-				if unknown != "":
-					output += "and "
+		# we have a lot of variables
+		certain, toStation, fromStation, journeyDate, journeyTime, adultTickets, childTickets = self.processQuery(query)
+		# strings to store outputs
+		uncertain = ""
+		unknown = ""
+		output = ""
+
+		# meow
+		for i, cat in enumerate(certain):
+
+			# if we're uncertain
+			if cat == False:
+				if i == 0:
+					uncertain += f"from {fromStation} "
+				elif i == 1:
+					uncertain += f"to {toStation} "
+				elif i == 2:
+					uncertain += f"on {journeyDate} "
+				elif i == 3:
+					uncertain += f"by {journeyTime} "
+				elif i == 4:
+					uncertain += f"for {adultTickets} adult(s) "
+				elif i == 5:
+					uncertain += f"for {childTickets} child(ren) "
+				elif i == 6:
+					returnStr = "youd like to return " if certain[6] else "you would not like to return"
+					uncertain += f"and {returnStr}"
+
+			# if we really have no idea
+			elif cat == None:
+				if i == 0:
+					unknown += "from which station "
+				elif i == 1:
+					unknown += "to which station "
+				elif i == 2:
+					unknown += "what date "
+				elif i == 3:
+					unknown += "by what time "
+				elif i == 4:
+					unknown += "how many adult(s) "
+				elif i == 5:
+					unknown += "how many child(ren) "
+				elif i == 6:
+					unknown += "would you like to return "
+
+		# double checck with the user
+		if uncertain != "":
+			output += ("just to check you want to go " + uncertain)
+
+			# if theres also something we don't know
 			if unknown != "":
-				output += ("would you mind providing more details, specifically: \n" + unknown)
-				
-			if output == "":
-				break
-				
-			else:
-				print(query)
-				query = input(output)
+				output += "and "
 
+		# make the user speak english
+		if unknown != "":
+			output += ("would you mind providing more details, specifically: " + unknown)
+			
+
+		# if there's nothing more to check then we should have everything
+		if output == "":
+
+			# format the date for the web scraping
+			formattedDate = journeyDate.strftime("%d %m %y")
+			day, month, year = formattedDate.split()
+
+			# format the time for the web scraping
+			formattedTime = journeyTime.strftime("%H %M")
+			hour, minute = formattedTime.split()
+
+			# round minute up to the next 15 minutes
+			minute = str(math.ceil(int(minute) / 15) * 15)
+
+			# TODO this code sucks and will break i need to do this a better way
+			if int(minute) >= 60:
+				minute = "00"
+				hour = str(int(hour) + 1)
+
+				if len(hour) == 1:
+					hour = "0" + hour
+
+			# get the url for the journey from all of our data
+			url = self.getUrl(fromStation, toStation, day, month, year, hour, minute)
+
+			# use web scraping to grab the price from that url
+			price = self.getPrice(url)
+			
+			# if we get a price, add it to the output
+			if price != None:
+				output += f"The cheapest ticket is {price}! "
+
+			# add the url to the output
+			output += f"You can get a ticket here: {url}"
 		
-		print(query)
-		return True
-
+		# we've figured out what to say, now say it
+		self.chatbotSpeech(output)
 				
 	# converts a variety of time strings to python time object
-	def to_time(self, time_str):
+	def toTime(self, timeStr):
 
 		# god only knows what this regex is actually doing
-		match = re.search(r'(\d{1,2}):(\d{2})(?:\s?(AM|PM))?', time_str, re.IGNORECASE)
+		match = re.search(r"(\d{1,2}):(\d{2})(?:\s?(AM|PM))?", timeStr, re.IGNORECASE)
 
 		# we got a time!
 		if match:
@@ -462,9 +519,9 @@ class TrainPredict:
 			hour, minute = int(hour), int(minute)
 
 			# handle am and pm
-			if period and period.lower() == 'pm' and hour != 12:
+			if period and period.lower() == "pm" and hour != 12:
 				hour += 12
-			elif period and period.lower() == 'am' and hour == 12:
+			elif period and period.lower() == "am" and hour == 12:
 				hour = 0
 
 			# send the time back!
@@ -478,9 +535,9 @@ class TrainPredict:
 	def getPrice(self, url):
 
 		# configure chromium
-		chrome_options = Options()
-		chrome_options.add_argument("--headless")
-		driver = webdriver.Chrome(options=chrome_options)
+		chromeOptions = Options()
+		chromeOptions.add_argument("--headless")
+		driver = webdriver.Chrome(options=chromeOptions)
 
 		# load our url
 		driver.get(url)
@@ -488,11 +545,14 @@ class TrainPredict:
 		# attempt to get price
 		try:
 
+			# id of box containing price
+			priceId = "jp-class-jp-results-standard"
+
 			# get the box containing the price
-			price_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "jp-class-jp-results-standard")))
+			priceBox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, priceId)))
 
 			# use regex to get the price from the div
-			return re.search(r"£\d+(\.\d+)?", price_box.get_attribute("aria-label")).group()
+			return re.search(r"£\d+(\.\d+)?", priceBox.get_attribute("aria-label")).group()
 
 		# just return nothing if web scraping fails
 		except:
@@ -500,17 +560,24 @@ class TrainPredict:
 
 	# get the url from national rail
 	def getUrl(self, origin, destination, date, month, year, hour, minute):
-		return f'https://www.nationalrail.co.uk/journey-planner/?type=single&origin={origin}&destination={destination}&leavingType=departing&leavingDate={date}{month}{year}&leavingHour={hour}&leavingMin={minute}&adults=1&extraTime=0#O'
+		return f"https://www.nationalrail.co.uk/journey-planner/?type=single&origin={origin}&destination={destination}&leavingType=departing&leavingDate={date}{month}{year}&leavingHour={hour}&leavingMin={minute}&adults=1&extraTime=0#O"
 		
-# TODO feels wrong to have this global
-trainPredict = TrainPredict()
-
 # entry point
 if __name__ == "__main__":
 
-	# run the chatbot in the terminal
-	terminal_thread = Thread(target=main)
-	terminal_thread.start()
+	# which port we wanna use
+	guiPort = 5001
+
+	# send link to gui
+	print(f"Gui is running on http://127.0.0.1:{guiPort}")
+
+	# start up trainPredict for the first time
+	trainPredict = TrainPredict()
+
+	# run the chatbot in the cli
+	cliThread = Thread(target=main)
+	cliThread.start()
 
 	# run the chatbot in the browser in main thread
-	socketio.run(app, port=5001)
+	socketio.run(app, port=guiPort)
+
